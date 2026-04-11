42fb3c84980dbfb1db7ac75287c739714fd19792
Chad Powell <brokkr.forgemaster+github@gmail.com>
2026-04-11 01:27:35 -0400

    update

---
+++ mypublish/wwwroot/swagger-ui/custom.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    // Change page title
+    document.title = "🐺 PackTracker API Docs";
+
+    // Add watermark to footer
+    const footer = document.createElement("div");
+    footer.style.cssText = "text-align:center; padding:10px; color:#F2E8DC; font-size:0.9em;";
+    footer.innerHTML = "House Wolf · PackTracker API · © 2025";
+    document.body.appendChild(footer);
+};
